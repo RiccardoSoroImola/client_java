b79bad283d274303cb78912fd3382377c18b88d8
Jenkins Automated Fix <jenkins@example.com>
2025-07-28 21:54:43 +0000

    Fix autogenerata per SonarQube issue 34680470-3e29-42e3-92fd-8b5169ac71a1: Generic exceptions should never be thrown

--- examples/example-exemplars-tail-sampling/example-greeting-service/src/main/java/io/prometheus/metrics/examples/otel/exemplars/greeting/GreetingServlet.java
+++ examples/example-exemplars-tail-sampling/example-greeting-service/src/main/java/io/prometheus/metrics/examples/otel/exemplars/greeting/GreetingServlet.java
@@ -39,7 +39,7 @@ public class GreetingServlet extends HttpServlet {
       resp.setContentType("text/plain");
       resp.getWriter().println("Hello, World!");
     } catch (InterruptedException e) {
-      throw new RuntimeException(e);
+      throw new IOException("Interrupted while sleeping", e);
     } finally {
       histogram.labelValues("200").observe(nanosToSeconds(System.nanoTime() - start));
     }
