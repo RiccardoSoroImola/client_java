4fb60ae4ddf0ca8eb8bc5af2c199ff2805b879ad
Jenkins Automated Fix <jenkins@example.com>
2025-07-28 16:51:41 +0000

    Fix autogenerata per SonarQube issue 8206e034-567f-4f09-beb4-4734174aab49: Deprecated code should be removed

--- prometheus-metrics-instrumentation-caffeine/src/main/java/io/prometheus/metrics/instrumentation/caffeine/CacheMetricsCollector.java
+++ prometheus-metrics-instrumentation-caffeine/src/main/java/io/prometheus/metrics/instrumentation/caffeine/CacheMetricsCollector.java
@@ -89,19 +89,6 @@ public class CacheMetricsCollector implements MultiCollector {
   private final boolean collectWeightedSize;
 
   /**
-   * Instantiates a {@link CacheMetricsCollector}, with the legacy parameters.
-   *
-   * <p>The use of this constructor is discouraged, in favor of a Builder pattern {@link #builder()}
-   *
-   * <p>Note that the {@link #builder()} API has different default values than this deprecated
-   * constructor.
-   */
-  @Deprecated
-  public CacheMetricsCollector() {
-    this(false, false);
-  }
-
-  /**
    * Instantiate a {@link CacheMetricsCollector}
    *
    * @param collectEvictionWeightAsCounter If true, {@code caffeine_cache_eviction_weight} will be
