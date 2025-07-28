445ae9f5e1a5553a2d70ada9bbe86dbe126c3ce1
Jenkins Automated Fix <jenkins@example.com>
2025-07-28 13:18:12 +0000

    Fix autogenerata per SonarQube issue 4f2d2f5c-5ad9-4017-8d98-75e37648ad05: Methods should not be empty

--- integration-tests/it-pushgateway/src/main/java/io/prometheus/metrics/it/pushgateway/PushGatewayTestApp.java
+++ integration-tests/it-pushgateway/src/main/java/io/prometheus/metrics/it/pushgateway/PushGatewayTestApp.java
@@ -85,10 +85,14 @@ class PushGatewayTestApp {
         }
 
         @Override
-        public void checkClientTrusted(X509Certificate[] chain, String authType) {}
+        public void checkClientTrusted(X509Certificate[] chain, String authType) {
+          // Intentionally empty for insecure trust manager
+        }
 
         @Override
-        public void checkServerTrusted(X509Certificate[] chain, String authType) {}
+        public void checkServerTrusted(X509Certificate[] chain, String authType) {
+          // Intentionally empty for insecure trust manager
+        }
       };
 
   static HttpConnectionFactory insecureConnectionFactory =
