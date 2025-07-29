73492adf50043ceaf7e453bd45b6837ea041c69b
Jenkins Automated Fix <jenkins@example.com>
2025-07-29 02:22:58 +0000

    Fix autogenerata per SonarQube issue e26f5209-bc68-4eba-8d99-2ee23bd0df03: Methods should not be empty

--- benchmarks/src/main/java/io/prometheus/metrics/benchmarks/TextFormatUtilBenchmark.java
+++ benchmarks/src/main/java/io/prometheus/metrics/benchmarks/TextFormatUtilBenchmark.java
@@ -105,18 +105,28 @@ public class TextFormatUtilBenchmark {
     }
 
     @Override
-    public void write(int b) {}
+    public void write(int b) {
+      // This method is intentionally empty as it represents a null output stream.
+    }
 
     @Override
-    public void write(byte[] b) {}
+    public void write(byte[] b) {
+      // This method is intentionally empty as it represents a null output stream.
+    }
 
     @Override
-    public void write(byte[] b, int off, int len) {}
+    public void write(byte[] b, int off, int len) {
+      // This method is intentionally empty as it represents a null output stream.
+    }
 
     @Override
-    public void flush() {}
+    public void flush() {
+      // This method is intentionally empty as it represents a null output stream.
+    }
 
     @Override
-    public void close() {}
+    public void close() {
+      // This method is intentionally empty as it represents a null output stream.
+    }
   }
 }
