ce7484db12a5d8dca14a6ffb03d04757e9810655
Jenkins Automated Fix <jenkins@example.com>
2025-07-29 11:13:25 +0000

    Fix autogenerata per SonarQube issue 317f36e5-e001-4a1a-b3c0-43568724ddf4: "InterruptedException" and "ThreadDeath" should not be ignored

--- examples/example-exporter-servlet-tomcat/src/main/java/io/prometheus/metrics/examples/tomcat_servlet/HelloWorldServlet.java
+++ examples/example-exporter-servlet-tomcat/src/main/java/io/prometheus/metrics/examples/tomcat_servlet/HelloWorldServlet.java
@@ -49,6 +49,7 @@ public class HelloWorldServlet extends HttpServlet {
       resp.setContentType("text/plain");
       resp.getWriter().println("Hello, World!");
     } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
       throw new RuntimeException(e);
     } catch (IOException e) {
       // Handle IOException to prevent it from being thrown from the servlet method
