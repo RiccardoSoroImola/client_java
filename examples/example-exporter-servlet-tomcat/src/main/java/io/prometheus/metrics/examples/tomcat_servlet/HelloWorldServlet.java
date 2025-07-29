dc87d82d65197d7c3f65f12f4cce35bcff5b5b1c
Jenkins Automated Fix <jenkins@example.com>
2025-07-29 03:51:21 +0000

    Fix autogenerata per SonarQube issue f10e377a-55fe-469a-b4de-79ea55215ce1: Fields in a "Serializable" class should either be transient or serializable

--- examples/example-exporter-servlet-tomcat/src/main/java/io/prometheus/metrics/examples/tomcat_servlet/HelloWorldServlet.java
+++ examples/example-exporter-servlet-tomcat/src/main/java/io/prometheus/metrics/examples/tomcat_servlet/HelloWorldServlet.java
@@ -12,22 +12,22 @@ import java.io.IOException;
 import java.util.Random;
 
 /** Hello World REST servlet, with an example counter and an example histogram. */
-public class HelloWorldServlet extends HttpServlet {
+public class HelloWorldServlet extends HttpServlet implements java.io.Serializable {
 
   private static final long serialVersionUID = 0L;
 
-  private final Random random = new Random(0);
+  private final transient Random random = new Random(0);
 
   // Note: The requests_total counter is not a great example, because the
   // request_duration_seconds histogram below also has a count with the number of requests.
-  private final Counter counter =
+  private final transient Counter counter =
       Counter.builder()
           .name("requests_total")
           .help("total number of requests")
           .labelNames("http_status")
           .register();
 
-  private final Histogram histogram =
+  private final transient Histogram histogram =
       Histogram.builder()
           .name("request_duration_seconds")
           .help("request duration in seconds")
