c1ec3e243a1c6950859baccd6fe845c4877267e7
Jenkins Automated Fix <jenkins@example.com>
2025-07-30 05:01:48 +0000

    Fix autogenerata per SonarQube issue fe1531ac-7e2b-4a16-b4a8-8ab9a37a5583: Lambdas should be replaced with method references

--- prometheus-metrics-config/src/test/java/io/prometheus/metrics/config/MetricsPropertiesTest.java
+++ prometheus-metrics-config/src/test/java/io/prometheus/metrics/config/MetricsPropertiesTest.java
@@ -49,21 +49,21 @@ class MetricsPropertiesTest {
     builder.summaryNumberOfAgeBuckets(0);
     MetricsProperties.Builder builderWithInvalidValue = builder;
     assertThatExceptionOfType(PrometheusPropertiesException.class)
-        .isThrownBy(() -> builderWithInvalidValue.build())
+        .isThrownBy(builderWithInvalidValue::build)
         .withMessage(".summaryNumberOfAgeBuckets: Expecting value > 0. Found: 0");
 
     MetricsProperties.Builder builder1 = MetricsProperties.builder();
     builder1.summaryQuantiles(2L);
     MetricsProperties.Builder builder1WithInvalidValue = builder1;
     assertThatExceptionOfType(PrometheusPropertiesException.class)
-        .isThrownBy(() -> builder1WithInvalidValue.build())
+        .isThrownBy(builder1WithInvalidValue::build)
         .withMessage(".summaryQuantiles: Expecting 0.0 <= quantile <= 1.0. Found: 2.0");
 
     MetricsProperties.Builder builder2 = MetricsProperties.builder();
     builder2.summaryQuantileErrors(0.9);
     MetricsProperties.Builder builder2WithInvalidValue = builder2;
     assertThatExceptionOfType(PrometheusPropertiesException.class)
-        .isThrownBy(() -> builder2WithInvalidValue.build())
+        .isThrownBy(builder2WithInvalidValue::build)
         .withMessage(
             ".summaryQuantileErrors: Can't configure summaryQuantileErrors without configuring"
                 + " summaryQuantiles");
@@ -73,7 +73,7 @@ class MetricsPropertiesTest {
     builder3.summaryQuantileErrors(0.1, 0.9);
     MetricsProperties.Builder builder3WithInvalidValue = builder3;
     assertThatExceptionOfType(PrometheusPropertiesException.class)
-        .isThrownBy(() -> builder3WithInvalidValue.build())
+        .isThrownBy(builder3WithInvalidValue::build)
         .withMessage(".summaryQuantileErrors: must have the same length as summaryQuantiles");
 
     MetricsProperties.Builder builder4 = MetricsProperties.builder();
@@ -81,7 +81,7 @@ class MetricsPropertiesTest {
     builder4.summaryQuantileErrors(-0.9);
     MetricsProperties.Builder builder4WithInvalidValue = builder4;
     assertThatExceptionOfType(PrometheusPropertiesException.class)
-        .isThrownBy(() -> builder4WithInvalidValue.build())
+        .isThrownBy(builder4WithInvalidValue::build)
         .withMessage(".summaryQuantileErrors: Expecting 0.0 <= error <= 1.0");
   }
 
@@ -122,7 +122,7 @@ class MetricsPropertiesTest {
     builder.histogramNativeInitialSchema(10);
     MetricsProperties.Builder builderWithInvalidValue = builder;
     assertThatExceptionOfType(PrometheusPropertiesException.class)
-        .isThrownBy(() -> builderWithInvalidValue.build())
+        .isThrownBy(builderWithInvalidValue::build)
         .withMessage(
             ".histogramNativeInitialSchema: Expecting number between -4 and +8. Found: 10");
 
@@ -130,28 +130,28 @@ class MetricsPropertiesTest {
     builder1.histogramNativeMinZeroThreshold(-1.0);
     MetricsProperties.Builder builder1WithInvalidValue = builder1;
     assertThatExceptionOfType(PrometheusPropertiesException.class)
-        .isThrownBy(() -> builder1WithInvalidValue.build())
+        .isThrownBy(builder1WithInvalidValue::build)
         .withMessage(".histogramNativeMinZeroThreshold: Expecting value >= 0. Found: -1.0");
 
     MetricsProperties.Builder builder2 = MetricsProperties.builder();
     builder2.histogramNativeMaxZeroThreshold(-1.0);
     MetricsProperties.Builder builder2WithInvalidValue = builder2;
     assertThatExceptionOfType(PrometheusPropertiesException.class)
-        .isThrownBy(() -> builder2WithInvalidValue.build())
+        .isThrownBy(builder2WithInvalidValue::build)
         .withMessage(".histogramNativeMaxZeroThreshold: Expecting value >= 0. Found: -1.0");
 
     MetricsProperties.Builder builder3 = MetricsProperties.builder();
     builder3.histogramNativeMaxNumberOfBuckets(-1);
     MetricsProperties.Builder builder3WithInvalidValue = builder3;
     assertThatExceptionOfType(PrometheusPropertiesException.class)
-        .isThrownBy(() -> builder3WithInvalidValue.build())
+        .isThrownBy(builder3WithInvalidValue::build)
         .withMessage(".histogramNativeMaxNumberOfBuckets: Expecting value >= 0. Found: -1");
 
     MetricsProperties.Builder builder4 = MetricsProperties.builder();
     builder4.histogramNativeResetDurationSeconds(-1L);
     MetricsProperties.Builder builder4WithInvalidValue = builder4;
     assertThatExceptionOfType(PrometheusPropertiesException.class)
-        .isThrownBy(() -> builder4WithInvalidValue.build())
+        .isThrownBy(builder4WithInvalidValue::build)
         .withMessage(".histogramNativeResetDurationSeconds: Expecting value >= 0. Found: -1");
 
     MetricsProperties.Builder builder5 = MetricsProperties.builder();
@@ -159,7 +159,7 @@ class MetricsPropertiesTest {
     builder5.histogramClassicOnly(true);
     MetricsProperties.Builder builder5WithInvalidValue = builder5;
     assertThatExceptionOfType(PrometheusPropertiesException.class)
-        .isThrownBy(() -> builder5WithInvalidValue.build())
+        .isThrownBy(builder5WithInvalidValue::build)
         .withMessage(".histogramNativeOnly and .histogramClassicOnly cannot both be true");
 
     MetricsProperties.Builder builder6 = MetricsProperties.builder();
@@ -167,7 +167,7 @@ class MetricsPropertiesTest {
     builder6.histogramNativeMaxZeroThreshold(0.01);
     MetricsProperties.Builder builder6WithInvalidValue = builder6;
     assertThatExceptionOfType(PrometheusPropertiesException.class)
-        .isThrownBy(() -> builder6WithInvalidValue.build())
+        .isThrownBy(builder6WithInvalidValue::build)
         .withMessage(
             ".histogramNativeMinZeroThreshold cannot be greater than"
                 + " .histogramNativeMaxZeroThreshold");
