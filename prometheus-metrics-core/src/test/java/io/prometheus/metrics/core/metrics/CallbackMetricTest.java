14fd515ffcec17cdb0e33cadb4931564347c420c
Jenkins Automated Fix <jenkins@example.com>
2025-07-30 00:26:03 +0000

    Fix autogenerata per SonarQube issue 7ea355c9-f47b-46c1-ad48-0fcbd979d3c2: Only one method invocation is expected when testing runtime exceptions

--- prometheus-metrics-core/src/test/java/io/prometheus/metrics/core/metrics/CallbackMetricTest.java
+++ prometheus-metrics-core/src/test/java/io/prometheus/metrics/core/metrics/CallbackMetricTest.java
@@ -8,43 +8,34 @@ class CallbackMetricTest {
 
   @Test
   void makeLabels() {
+    CounterWithCallback obj =
+        CounterWithCallback.builder()
+            .name("c")
+            .callback(callback -> {})
+            .labelNames("label1", "label2")
+            .build();
     assertThatExceptionOfType(IllegalArgumentException.class)
-        .isThrownBy(
-            () -> {
-              CounterWithCallback obj =
-                  CounterWithCallback.builder()
-                      .name("c")
-                      .callback(callback -> {})
-                      .labelNames("label1", "label2")
-                      .build();
-              obj.makeLabels("foo");
-            })
+        .isThrownBy(() -> obj.makeLabels("foo"))
         .withMessage(
             "CounterWithCallback was created with 2 label names, but the callback was called with 1"
                 + " label values.");
 
+    CounterWithCallback obj2 =
+        CounterWithCallback.builder()
+            .name("c")
+            .callback(callback -> {})
+            .labelNames("label1", "label2")
+            .build();
     assertThatExceptionOfType(IllegalArgumentException.class)
-        .isThrownBy(
-            () -> {
-              CounterWithCallback obj =
-                  CounterWithCallback.builder()
-                      .name("c")
-                      .callback(callback -> {})
-                      .labelNames("label1", "label2")
-                      .build();
-              obj.makeLabels((String[]) null);
-            })
+        .isThrownBy(() -> obj2.makeLabels((String[]) null))
         .withMessage(
             "CounterWithCallback was created with label names, but the callback was called without"
                 + " label values.");
 
+    CounterWithCallback obj3 =
+        CounterWithCallback.builder().name("c").callback(callback -> {}).build();
     assertThatExceptionOfType(IllegalArgumentException.class)
-        .isThrownBy(
-            () -> {
-              CounterWithCallback obj =
-                  CounterWithCallback.builder().name("c").callback(callback -> {}).build();
-              obj.makeLabels("foo");
-            })
+        .isThrownBy(() -> obj3.makeLabels("foo"))
         .withMessage(
             "Cannot pass label values to a CounterWithCallback that was created without label"
                 + " names.");
