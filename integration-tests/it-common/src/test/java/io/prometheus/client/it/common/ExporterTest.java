b398cc4f859639f94b7a33f535c04882ea6ef80e
Jenkins Automated Fix <jenkins@example.com>
2025-07-31 00:36:50 +0000

    Fix autogenerata per SonarQube issue ad22e999-bbbe-40e3-b5d1-bf43da4508e9: "@Deprecated" code should not be used

--- integration-tests/it-common/src/test/java/io/prometheus/client/it/common/ExporterTest.java
+++ integration-tests/it-common/src/test/java/io/prometheus/client/it/common/ExporterTest.java
@@ -89,12 +89,12 @@ public abstract class ExporterTest {
           return new Response(
               con.getResponseCode(),
               con.getHeaderFields(),
-              IOUtils.toByteArray(con.getInputStream(), 4096));
+              IOUtils.toByteArray(con.getInputStream()));
         } else {
           return new Response(
               con.getResponseCode(),
               con.getHeaderFields(),
-              IOUtils.toByteArray(con.getErrorStream(), 4096));
+              IOUtils.toByteArray(con.getErrorStream()));
         }
       } catch (Exception e) {
         exception = e;
@@ -147,7 +147,7 @@ public abstract class ExporterTest {
 
     public String gzipBody() throws IOException {
       return new String(
-          IOUtils.toByteArray(new GZIPInputStream(new ByteArrayInputStream(body)), 4096), UTF_8);
+          IOUtils.toByteArray(new GZIPInputStream(new ByteArrayInputStream(body))), UTF_8);
     }
 
     public List<Metrics.MetricFamily> protoBody() throws IOException {
