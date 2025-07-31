2ab8f6ef20d37c70e334e7cf18445a7f5531254e
Jenkins Automated Fix <jenkins@example.com>
2025-07-31 03:50:26 +0000

    Fix autogenerata per SonarQube issue 287377d2-6951-4cf2-bf1d-5149b674d567: Redundant casts should not be used

--- prometheus-metrics-instrumentation-dropwizard5/src/main/java/io/prometheus/metrics/instrumentation/dropwizard5/DropwizardExports.java
+++ prometheus-metrics-instrumentation-dropwizard5/src/main/java/io/prometheus/metrics/instrumentation/dropwizard5/DropwizardExports.java
@@ -111,7 +111,7 @@ public class DropwizardExports implements MultiCollector {
   MetricSnapshot fromCounter(String dropwizardName, Counter counter) {
     MetricMetadata metadata = getMetricMetaData(dropwizardName, counter);
     CounterSnapshot.CounterDataPointSnapshot.Builder dataPointBuilder =
-        CounterSnapshot.CounterDataPointSnapshot.builder().value((double) counter.getCount());
+        CounterSnapshot.CounterDataPointSnapshot.builder().value(counter.getCount());
     labelMapper.ifPresent(
         mapper ->
             dataPointBuilder.labels(
