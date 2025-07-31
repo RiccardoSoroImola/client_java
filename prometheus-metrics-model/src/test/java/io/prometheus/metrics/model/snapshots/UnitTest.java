1feed634399675315c6d1dbc2404a673f5f19d99
Jenkins Automated Fix <jenkins@example.com>
2025-07-31 07:12:11 +0000

    Fix autogenerata per SonarQube issue 06a694bc-d81b-482b-beed-eb9df6806f48: Similar tests should be grouped in a single Parameterized test

--- prometheus-metrics-model/src/test/java/io/prometheus/metrics/model/snapshots/UnitTest.java
+++ prometheus-metrics-model/src/test/java/io/prometheus/metrics/model/snapshots/UnitTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class UnitTest {
 
@@ -12,34 +14,11 @@ class UnitTest {
     assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new Unit(" "));
   }
 
-  @Test
-  void testEquals1() {
-    Unit unit1 = Unit.BYTES;
-    Unit unit2 = new Unit("bytes");
-
-    assertThat(unit1).isEqualTo(unit2);
-  }
-
-  @Test
-  void testEquals2() {
-    Unit unit1 = new Unit("bytes ");
-    Unit unit2 = new Unit("bytes");
-
-    assertThat(unit1).isEqualTo(unit2);
-  }
-
-  @Test
-  void testEquals3() {
-    Unit unit1 = new Unit(" bytes");
-    Unit unit2 = new Unit("bytes");
-
-    assertThat(unit1).isEqualTo(unit2);
-  }
-
-  @Test
-  void testEquals4() {
-    Unit unit1 = new Unit(" bytes ");
-    Unit unit2 = new Unit("bytes");
+  @ParameterizedTest
+  @CsvSource({"bytes, bytes", "bytes , bytes", " bytes, bytes", " bytes , bytes"})
+  void testEquals(String input1, String input2) {
+    Unit unit1 = new Unit(input1);
+    Unit unit2 = new Unit(input2);
 
     assertThat(unit1).isEqualTo(unit2);
   }
