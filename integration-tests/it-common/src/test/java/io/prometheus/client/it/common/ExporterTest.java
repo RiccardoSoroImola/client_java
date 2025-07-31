ba944019e65176acf33f58c8ed54550f0e48d482
Jenkins Automated Fix <jenkins@example.com>
2025-07-31 03:44:15 +0000

    Fix autogenerata per SonarQube issue ad22e999-bbbe-40e3-b5d1-bf43da4508e9: "@Deprecated" code should not be used

--- integration-tests/it-common/src/test/java/io/prometheus/client/it/common/ExporterTest.java
+++ integration-tests/it-common/src/test/java/io/prometheus/client/it/common/ExporterTest.java
@@ -89,12 +89,12 @@ public abstract class ExporterTest {
           return new Response(
               con.getResponseCode(),
               con.getHeaderFields(),
-              IOUtils.toByteArray(con.getInputStream()));
+              IOUtils.toByteArray(con.getInputStream(), 4096));
         } else {
           return new Response(
               con.getResponseCode(),
               con.getHeaderFields(),
-              IOUtils.toByteArray(con.getErrorStream()));
+              IOUtils.toByteArray(con.getErrorStream(), 4096));
         }
       } catch (Exception e) {
         exception = e;
